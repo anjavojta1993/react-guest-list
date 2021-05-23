1f37705dbb84ff4df8d1c4a754e5715d8c346019
anjavojta1993 <anja.vojta@gmail.com>
2021-05-23 11:38:27 +0200

    Update onclick button

--- src/components/GuestInput.js
+++ src/components/GuestInput.js
@@ -1,30 +1,58 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { useEffect } from 'react';
+
 export default function GuestInput({
   firstName,
   lastName,
   setFirstName,
   setLastName,
+  setCreatedGuest,
 }) {
+  useEffect(() => {
+    async function addGuest() {
+      const baseUrl = 'http://localhost:5000';
+      try {
+        const response = await fetch(`${baseUrl}/`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            firstName: firstName,
+            lastName: lastName,
+          }),
+        });
+        setCreatedGuest(await response.json());
+      } catch (e) {
+        console.error(e);
+      }
+    }
+
+    /* const handleAddGuest = () => {
+      setFirstName(firstName);
+      setLastName(lastName);
+      addGuest();
+    }; */
+  }, [firstName, lastName, setCreatedGuest, setFirstName, setLastName]);
+
   return (
     <div className="Inputs">
-      <label htmlFor="First name">
-        First name:
-        <input
-          type="text"
-          value={firstName}
-          onChange={(e) => setFirstName(e.currentTarget.value)}
-        />
-      </label>
+      <input
+        type="text"
+        placeholder="First name"
+        value={firstName}
+        onChange={(e) => setFirstName(e.currentTarget.value)}
+      />
       <br />
-
-      <label htmlFor="Last name">
-        Last name:
-        <input
-          type="text"
-          value={lastName}
-          onChange={(e) => setLastName(e.currentTarget.value)}
-        />
-      </label>
+      <input
+        type="text"
+        placeholder="Last name"
+        value={lastName}
+        onChange={(e) => setLastName(e.currentTarget.value)}
+      />
       <br />
+      {/* <button onClick={handleAddGuest}>Add Guest</button>; */}
     </div>
   );
 }
