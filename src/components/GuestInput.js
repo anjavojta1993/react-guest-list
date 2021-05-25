5c66d4d160393d88e15265692f13b92a86ec34a6
anjavojta1993 <anja.vojta@gmail.com>
2021-05-25 22:34:36 +0200

    Change onclick add guest

--- src/components/GuestInput.js
+++ src/components/GuestInput.js
@@ -6,27 +6,24 @@ export default function GuestInput({
   handleAddGuest,
 }) {
   return (
-    <div className="Inputs">
-      <form>
-        <input
-          type="text"
-          placeholder="First name"
-          value={firstName}
-          onChange={(e) => setFirstName(e.currentTarget.value)}
-        />
-        <br />
-        <input
-          type="text"
-          placeholder="Last name"
-          value={lastName}
-          onChange={(e) => setLastName(e.currentTarget.value)}
-        />
-        <br />
-        <button type="submit" onClick={handleAddGuest}>
-          Add Guest
-        </button>
-        ;
-      </form>
-    </div>
+    <form className="Inputs">
+      <input
+        type="text"
+        placeholder="First name"
+        value={firstName}
+        onChange={(e) => setFirstName(e.currentTarget.value)}
+      />
+      <br />
+      <input
+        type="text"
+        placeholder="Last name"
+        value={lastName}
+        onChange={(e) => setLastName(e.currentTarget.value)}
+      />
+      <br />
+      <button type="submit" onClick={handleAddGuest}>
+        Add Guest
+      </button>
+    </form>
   );
 }
